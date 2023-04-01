c92468ee8eb52b0cfb37fe3980d3a350130410bc
Camden DeVoogd <camdendevoogd@gmail.com>
2023-03-31 22:17:46 -0600

    Output includes up to 1 level deep

--- include.py
+++ include.py
@@ -2,6 +2,7 @@
 
 import argparse
 import fileinput
+import os
 import re
 
 # This regular expression is looking for:
@@ -14,24 +15,33 @@ regex = re.compile(r"^#include \"(.+)\"\s*$")
 def parse_arguments() -> argparse.Namespace:
     p = argparse.ArgumentParser()
     p.add_argument(
-        "files",
-        nargs="?",
+        "file",
         default="-",
     )
     return p.parse_args()
 
 
+def raw_print(line: object):
+    print(line, end="")
+
+
 def main():
     args = parse_arguments()
 
-    with fileinput.input(files=args.files) as f:
+    with fileinput.input(files=args.file) as f:
         for line in f:
             match = regex.match(line)
             if not match:
+                raw_print(line)
                 continue
 
-            print(f"Match Found! {fileinput.filename()} @ L{fileinput.lineno()}")
-            print(f"    Included File: {match.group(1)}")
+            current_file = fileinput.filename()
+            base_dir = os.path.dirname(current_file)
+            included_path = match.group(1)
+            to_include = os.path.join(base_dir, included_path)
+
+            with open(to_include, "r") as included:
+                raw_print(included.read())
 
 
 if __name__ == "__main__":
